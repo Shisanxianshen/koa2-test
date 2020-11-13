46021288c6d77736fee85b6724dd95efb0115c4d
ymf961016 <1161015103@qq.com>
2020-11-13 13:58:12 +0800

    邮件信息更改

--- sendMail/index.js
+++ sendMail/index.js
@@ -2,9 +2,10 @@
 const nodemailer = require("nodemailer")
 let transporter = nodemailer.createTransport({
   host:'smtp.163.com',//使用内置的163发送邮件
+  secure: true,
   auth: {
     user: "15163575932@163.com",
-    pass: "SJPLBCHBKHIIQCLF",
+    pass: "VSFGFMZVREDBZHNQ",
   },
 })
 // 邮件服务器准备
@@ -15,15 +16,6 @@ transporter.verify(function (error, success) {
     console.log('Server is ready')
   }
 })
-
-// transporter.sendMail({
-//   from:'15163575932@163.com',
-//   to:'1161015103@qq.com',
-//   subject:'dmg的个人小站邮箱验证码',
-//   text:`请在60s内填写邮箱验证码，${433100}`,
-// },function(){
-//   console.log('邮件已发送')
-// })
 
 module.exports = transporter
   
